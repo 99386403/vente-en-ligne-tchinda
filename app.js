d8e3776d15d850cb4659678065338c4597935a85
Vente En Ligne TCHINDA <tchindaentreprise@gmail.com>
2025-06-28 09:59:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
-// ✅ Liste des produits avec images réelles let produits = [ { identifiant: 101, nom: "Article 01", prix: 5000, categories: "Chaussures", images: ["https://i.ibb.co/BnZVCht/article01.jpg"] }, { identifiant: 102, nom: "Article 02", prix: 10000, categories: "Chaussures", images: ["https://i.ibb.co/M87tHb6/article02.jpg"] }, { identifiant: 103, nom: "Article 03", prix: 20000, categories: "Chaussures", images: ["https://i.ibb.co/BK3WNCT/article03.jpg"] }, { identifiant: 104, nom: "Article 04", prix: 17000, categories: "Chaussures", images: ["https://i.ibb.co/WpX98Cg/article04.jpg"] }, { identifiant: 105, nom: "Article 05", prix: 17000, categories: "Chaussures", images: ["https://i.ibb.co/h9Dzjrg/article05.jpg"] }, { identifiant: 106, nom: "Article 06", prix: 15000, categories: "Chaussures", images: ["https://i.ibb.co/zPJ7Nmp/article06.jpg"] } ];
 
-let panier = []; let favoris = [];
+let produits = [
+  {
+    identifiant: 1,
+    nom: "Article 01",
+    prix: 5000,
+    categories: "Chaussures",
+    images: ["https://i.ibb.co/BnZVCht/article01.jpg"]
+  },
+  {
+    identifiant: 2,
+    nom: "Article 02",
+    prix: 10000,
+    categories: "Chaussures",
+    images: ["https://i.ibb.co/vmhCc3h/article02.jpg"]
+  },
+  {
+    identifiant: 3,
+    nom: "Article 03",
+    prix: 20000,
+    categories: "Chaussures",
+    images: ["https://i.ibb.co/KDwS3NK/article03.jpg"]
+  },
+  {
+    identifiant: 4,
+    nom: "Article 04",
+    prix: 17000,
+    categories: "Chaussures",
+    images: ["https://i.ibb.co/D8fZVNd/article04.jpg"]
+  },
+  {
+    identifiant: 5,
+    nom: "Article 05",
+    prix: 17000,
+    categories: "Chaussures",
+    images: ["https://i.ibb.co/Vxvt6Mv/article05.jpg"]
+  },
+  {
+    identifiant: 6,
+    nom: "Article 06",
+    prix: 15000,
+    categories: "Chaussures",
+    images: ["https://i.ibb.co/GQcc2Dw/article06.jpg"]
+  }
+];
 
+let panier = [];
+let favoris = [];
 
