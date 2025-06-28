1e9b6141c11f5719aa3117ef5dcaf947094d6ca5
Vente En Ligne TCHINDA <tchindaentreprise@gmail.com>
2025-06-28 09:51:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+// ✅ Liste des produits avec images réelles let produits = [ { identifiant: 101, nom: "Article 01", prix: 5000, categories: "Chaussures", images: ["https://i.ibb.co/BnZVCht/article01.jpg"] }, { identifiant: 102, nom: "Article 02", prix: 10000, categories: "Chaussures", images: ["https://i.ibb.co/M87tHb6/article02.jpg"] }, { identifiant: 103, nom: "Article 03", prix: 20000, categories: "Chaussures", images: ["https://i.ibb.co/BK3WNCT/article03.jpg"] }, { identifiant: 104, nom: "Article 04", prix: 17000, categories: "Chaussures", images: ["https://i.ibb.co/WpX98Cg/article04.jpg"] }, { identifiant: 105, nom: "Article 05", prix: 17000, categories: "Chaussures", images: ["https://i.ibb.co/h9Dzjrg/article05.jpg"] }, { identifiant: 106, nom: "Article 06", prix: 15000, categories: "Chaussures", images: ["https://i.ibb.co/zPJ7Nmp/article06.jpg"] } ];
+
+let panier = []; let favoris = [];
+
 
